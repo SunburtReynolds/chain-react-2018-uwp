f0df9f0120cff640594461872edfd8ddab5bcc78
Jon Reynolds <jon.reynolds456@gmail.com>
2018-07-11 10:17:41 -0700

    first ml update

--- src/App.tsx
+++ src/App.tsx
@@ -3,39 +3,15 @@ import { StyleSheet, View } from 'react-native';
 import { TabNavigator } from 'react-navigation';
 import { WelcomeScreen } from './screens/welcomeScreen';
 import { CodePushScreen } from './screens/codePushScreen';
-import { BuildScreen } from './screens/buildScreen';
-import { TestScreen } from './screens/testScreen';
-import { DistributeScreen } from './screens/distributeScreen';
-import { CrashesScreen } from './screens/crashesScreen';
-import { AnalyticsScreen } from './screens/analyticsScreen';
-import { PushScreen } from './screens/pushScreen';
 
 const RootTabNavigator = TabNavigator(
   {
     Welcome: {
       screen: WelcomeScreen,
     },
-    Build: {
-      screen: BuildScreen,
-    },
-    Test: {
-      screen: TestScreen,
-    },
     CodePush: {
       screen: CodePushScreen
     },
-    Distribute: {
-      screen: DistributeScreen,
-    },
-    Crashes: {
-      screen: CrashesScreen,
-    },
-    Analytics: {
-      screen: AnalyticsScreen,
-    },
-    Push: {
-      screen: PushScreen,
-    }
   },
   {
     initialRouteName: 'Welcome',
--- src/screens/welcomeScreen.tsx
+++ src/screens/welcomeScreen.tsx
@@ -1,28 +1,93 @@
-import * as React from 'react';
-import { View, Text, StyleSheet, Image, Platform } from 'react-native';
-import BaseScreen from '../components/baseScreen';
-import images from '../images';
+import * as React from "react";
+import { View, Text, StyleSheet, Image, Platform } from "react-native";
+import BaseScreen from "../components/baseScreen";
+import images from "../images";
+import Camera from "react-native-camera";
+import ImageRecognizer from "../ImageRecognizer";
 
 export class WelcomeScreen extends React.Component {
-    render() {
-        return (
-            <View style={{ flex: 1 }} testID="welcomeScreen" accessibilityLabel={"welcomeScreen"} accessible={true}>
-                <BaseScreen options={
-                    {
-                        title: "Welcome",
-                        topContainer: {
-                            height: 150,
-                            backgroundColor: "#CB2E62",
-                            imageSource: images.aclogo
-                        },
-                        bottomContainer: {
-                            backgroundColor: "#252525",
-                            description: "Visual Studio App Center is mission control for apps. It brings together multiple services, commonly used for mobile developers, into a single, integrated product.\n\n\nSwipe right to learn about our services"
-                        }
-                    }
-                }>
-                </BaseScreen>
-            </View>
-        );
+  camera: any;
+  recognizer: any;
+  componentDidMount() {
+    this.recognizer = new ImageRecognizer({
+      model: require("../../assets/model.pb"),
+      labels: require("../../assets/labels.txt")
+    });
+  }
+  render() {
+    return (
+      <View
+        style={{ flex: 1 }}
+        testID="welcomeScreen"
+        accessibilityLabel={"welcomeScreen"}
+        accessible={true}
+      >
+        <Camera
+          ref={cam => {
+            this.camera = cam;
+          }}
+          captureTarget={
+            Platform.OS == "ios"
+              ? Camera.constants.CaptureTarget.disk
+              : Camera.constants.CaptureTarget.cameraRoll
+          }
+          style={styles.preview}
+          aspect={Camera.constants.Aspect.fill}
+        />
+        <Text style={styles.capture} onPress={this.takePicture.bind(this)}>
+          Capture
+        </Text>
+      </View>
+    );
+  }
+  async takePicture() {
+    const options = {};
+    try {
+      const data = await this.camera.capture({
+        metadata: options,
+        captureTarget: Camera.constants.CaptureTarget.temp,
+        captureQuality: Camera.constants.CaptureQuality["480p"]
+      });
+      if (data) {
+        // alert(data.path);
+        const results = await this.recognizer.recognize({
+          image: data.path,
+          inputName: "Placeholder",
+          outputName: "loss"
+        });
+        if (results.length > 0) {
+          alert(
+            `Name: ${
+              results[0].name
+            } - Confidence: ${results[0].confidence.toFixed(2)}`
+          );
+        } else {
+          alert("Nothing happened");
+        }
+      }
+    } catch (err) {
+      alert(err);
     }
+  }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: "row"
+  },
+  preview: {
+    flex: 1,
+    justifyContent: "flex-end",
+    alignItems: "center"
+  },
+  capture: {
+    position: "absolute",
+    bottom: 0,
+    backgroundColor: "#fff",
+    borderRadius: 5,
+    color: "#000",
+    padding: 10,
+    margin: 40
+  }
+});
